05acb1456e7bff5dd0a6d53fadf86960455c3c1e
sewerynzagajny <seweryn.zagajny@gmail.com>
2025-05-08 11:42:03 +0200

    0805v2

--- src/pages/Assortment.js
+++ src/pages/Assortment.js
@@ -2,30 +2,49 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import ScrollEffectContainer from "../components/ScrollEffectContainer";
 import Btn from "../components/Btn";
+import { ReactComponent as AllegroIcon } from "../svg/full-shoping-cart-svgrepo-com.svg";
+import { ReactComponent as EtsyIcon } from "../svg/etsy-logo-svgrepo-com.svg";
 
 export default function Assortment() {
   return (
     <section className="assortment">
       <Navigation />
 
-      <ScrollEffectContainer
-        threshold={0}
-        animationTime={0.6}
-        animationTransform="translateY(2rem)"
-        rootMargin="50%"
-      >
-        <div className="assortment__container">
+      <div className="assortment__container">
+        <ScrollEffectContainer
+          threshold={0}
+          animationTime={0.6}
+          animationTransform="translateY(2rem)"
+          rootMargin="50%"
+        >
           <h2 className="heading-secondary">Asortyment</h2>
           <h3 className="heading-tertiary">
             <q>Wybierz platformę sprzedażową</q>
           </h3>
           <div className="assortment__container__btns">
-            <Btn href="https://www.etsy.com/pl/shop/MaGo3dPL">Etsy</Btn>
-            <Btn href="https://allegro.pl/uzytkownik/MaGo3d">Allegro</Btn>
+            <Btn
+              className="btn btn_href assortment__container__btns__shop-btn"
+              href="https://www.etsy.com/pl/shop/MaGo3dPL"
+            >
+              {" "}
+              <EtsyIcon className="assortment__container__btns__shop-btn--link" />
+              <span className="assortment__container__btns__shop-btn--text-etsy">
+                tsy
+              </span>
+            </Btn>
+            <Btn
+              href="https://allegro.pl/uzytkownik/MaGo3d"
+              className="btn btn_href assortment__container__btns__shop-btn"
+            >
+              <AllegroIcon className="assortment__container__btns__shop-btn--link" />{" "}
+              <span className="assortment__container__btns__shop-btn--text-allegro">
+                Allegro
+              </span>
+            </Btn>
           </div>
-        </div>
-        <Footer />
-      </ScrollEffectContainer>
+        </ScrollEffectContainer>
+      </div>
+      <Footer />
     </section>
   );
 }
