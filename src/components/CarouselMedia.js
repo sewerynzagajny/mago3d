2afd386ac5a80d5dca79a59773d7917f0009f38d
sewerynzagajny <seweryn.zagajny@gmail.com>
2025-06-13 10:16:16 +0200

    1306v2

--- src/components/CarouselMedia.js
+++ src/components/CarouselMedia.js
@@ -1,3 +1,4 @@
+import ScrollEffectContainer from "./ScrollEffectContainer";
 export default function CarouselMedia({
   item,
   zoomed,
@@ -10,17 +11,66 @@ export default function CarouselMedia({
 }) {
   if (item.type === "video") {
     return (
-      <div className={isModal ? "" : "video-carousel-frame"}>
+      <ScrollEffectContainer
+        totalImages={0}
+        threshold={0}
+        animationTime={isModal ? 0 : 0.6}
+        animationTransform="translateY(0rem)"
+        rootMargin="50%"
+      >
+        <div className={isModal ? "" : "video-carousel-frame"}>
+          <figure>
+            <video
+              controls
+              className={`carousel__media--video${
+                zoomed ? " carousel__media--zoomed" : ""
+              }`}
+              autoPlay
+              loop
+              muted
+              playsInline
+              style={{
+                ...(isModal
+                  ? {
+                      maxHeight: zoomed ? "auto" : "98vh",
+                      width: "100%",
+                      transform: zoomed
+                        ? `scale(${scale}) translate(${drag.x / scale}px, ${
+                            drag.y / scale
+                          }px)`
+                        : "none",
+                      zIndex: zoomed ? 10001 : "auto",
+                    }
+                  : {}),
+                ...cursorStyle,
+                cursor: zoomed && dragging ? "grabbing" : cursorStyle.cursor,
+                pointerEvents: "none",
+              }}
+            >
+              <source src={item.src} type="video/mp4" />
+              Twoja przeglądarka nie obsługuje wideo.
+            </video>
+          </figure>
+        </div>
+      </ScrollEffectContainer>
+    );
+  }
+  return (
+    <ScrollEffectContainer
+      totalImages={1}
+      threshold={0}
+      animationTime={isModal ? 0 : 0.6}
+      animationTransform="translateY(0rem)"
+      rootMargin="50%"
+    >
+      <div className={isModal ? "" : "photo-carousel-frame"}>
         <figure>
-          <video
-            controls
-            className={`carousel__media--video${
+          <img
+            src={item.src}
+            alt={item.alt}
+            className={`carousel__media--image${
               zoomed ? " carousel__media--zoomed" : ""
             }`}
-            autoPlay
-            loop
-            muted
-            playsInline
             style={{
               ...(isModal
                 ? {
@@ -36,44 +86,11 @@ export default function CarouselMedia({
                 : {}),
               ...cursorStyle,
               cursor: zoomed && dragging ? "grabbing" : cursorStyle.cursor,
-              pointerEvents: "none",
+              pointerEvents: "auto",
             }}
-          >
-            <source src={item.src} type="video/mp4" />
-            Twoja przeglądarka nie obsługuje wideo.
-          </video>
+          />
         </figure>
       </div>
-    );
-  }
-  return (
-    <div className={isModal ? "" : "photo-carousel-frame"}>
-      <figure>
-        <img
-          src={item.src}
-          alt={item.alt}
-          className={`carousel__media--image${
-            zoomed ? " carousel__media--zoomed" : ""
-          }`}
-          style={{
-            ...(isModal
-              ? {
-                  maxHeight: zoomed ? "auto" : "98vh",
-                  width: "100%",
-                  transform: zoomed
-                    ? `scale(${scale}) translate(${drag.x / scale}px, ${
-                        drag.y / scale
-                      }px)`
-                    : "none",
-                  zIndex: zoomed ? 10001 : "auto",
-                }
-              : {}),
-            ...cursorStyle,
-            cursor: zoomed && dragging ? "grabbing" : cursorStyle.cursor,
-            pointerEvents: "auto",
-          }}
-        />
-      </figure>
-    </div>
+    </ScrollEffectContainer>
   );
 }
--- src/pages/assortyment/Tsv3.js
+++ src/pages/assortyment/Tsv3.js
@@ -5,6 +5,7 @@ import CookieBaner from "../../components/CookieBanner";
 import Carousel from "../../components/Carousel";
 import Product from "../../components/Product";
 import { products } from "../../data/products";
+import ScrollEffectContainer from "../../components/ScrollEffectContainer";
 
 const items = [
   {
@@ -65,164 +66,173 @@ export default function Tsv3() {
     <>
       <section className="details">
         <Navigation />
-        <div className="details__container">
-          <h2 className="heading-secondary">Szczegóły</h2>
-          <h3 className="heading-tertiary">{products[1].name} </h3>
-          <div className="details__container__content ">
-            {(onMenuVisible || orderModalProductId !== null) && (
-              <div
-                style={{
-                  position: "absolute",
-                  zIndex: 100,
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: "100%",
-                  background: "rgba(0,0,0,0.4)",
-                  // cursor: "pointer",
-                }}
-                onClick={() => setOrderModalProductId(null)}
-              />
-            )}
-            <Carousel
-              items={items}
-              onItemClick={(idx) => {
-                setModalIndex(idx);
-                setModalOpen(true);
-                setZoomed(false); // reset powiększenia przy otwarciu
-              }}
-            />
-            {/* MODAL */}
-            {modalOpen && (
-              <div
-                className={`carousel__modal${
-                  zoomed ? " carousel__modal--zoomed" : ""
-                }`}
-                onClick={handleModalClick}
-              >
-                <button
-                  className="carousel__modal__close-btn"
-                  onClick={() => {
-                    setModalOpen(false);
-                    setZoomed(false);
+        <ScrollEffectContainer
+          threshold={0}
+          animationTime={0.6}
+          animationTransform="translateY(2rem)"
+          rootMargin="50%"
+        >
+          <div className="details__container">
+            <h2 className="heading-secondary">Szczegóły</h2>
+            <h3 className="heading-tertiary">{products[1].name} </h3>
+            <div className="details__container__content ">
+              {(onMenuVisible || orderModalProductId !== null) && (
+                <div
+                  style={{
+                    position: "absolute",
+                    zIndex: 100,
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    background: "rgba(0,0,0,0.4)",
+                    // cursor: "pointer",
                   }}
-                  aria-label="Zamknij"
-                >
-                  &times;
-                </button>
-                <div>
-                  <Carousel
-                    items={items}
-                    initialIndex={modalIndex}
-                    isModal
-                    onItemClick={() => setZoomed((z) => !z)}
-                    zoomed={zoomed}
-                    onResetZoom={() => setZoomed(false)}
-                    onClose={() => {
-                      setModalOpen(false);
-                      setZoomed(false);
-                    }}
-                  />
-                </div>
-              </div>
-            )}
-            <div className="details__sticky-product">
-              <Product
-                key={1}
-                product={products[1]}
-                className="assortment__container__products__item"
-                onMenuChange={setOnMenuVisible}
-                inDetails={true}
-                setOrderModalVisible={(visible) =>
-                  setOrderModalProductId(visible ? products[1].id : null)
-                }
-                orderModalVisible={orderModalProductId === products[1].id}
-                style={
-                  onMenuVisible || orderModalProductId !== null
-                    ? { opacity: "0.8" }
-                    : {}
-                }
+                  onClick={() => setOrderModalProductId(null)}
+                />
+              )}
+
+              <Carousel
+                items={items}
+                onItemClick={(idx) => {
+                  setModalIndex(idx);
+                  setModalOpen(true);
+                  setZoomed(false); // reset powiększenia przy otwarciu
+                }}
               />
-            </div>
-            <div className="details__container__content__text">
-              <p>
-                Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
-                ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
-                Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
-                przestrzeni roboczej.
-              </p>
-              <p>
-                Wykonana z wysokiej jakości materiałów, podstawka jest trwała i
-                łatwa do czyszczenia. Jej nowoczesny design sprawia, że
-                doskonale komponuje się z każdym wnętrzem.
-              </p>
-              <p>
-                Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą i
-                funkcjonalnością, które ułatwią Ci codzienne gotowanie.
-              </p>
-              <p>
-                Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
-                ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
-                Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
-                przestrzeni roboczej.
-              </p>
-              <p>
-                Wykonana z wysokiej jakości materiałów, podstawka jest trwała i
-                łatwa do czyszczenia. Jej nowoczesny design sprawia, że
-                doskonale komponuje się z każdym wnętrzem.
-              </p>
-              <p>
-                Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą i
-                funkcjonalnością, które ułatwią Ci codzienne gotowanie.
-              </p>
-              <p>
-                Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
-                ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
-                Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
-                przestrzeni roboczej.
-              </p>
-              <p>
-                Wykonana z wysokiej jakości materiałów, podstawka jest trwała i
-                łatwa do czyszczenia. Jej nowoczesny design sprawia, że
-                doskonale komponuje się z każdym wnętrzem.
-              </p>
-              <p>
-                Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą i
-                funkcjonalnością, które ułatwią Ci codzienne gotowanie.
-              </p>
-              <p>
-                Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
-                ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
-                Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
-                przestrzeni roboczej.
-              </p>
-              <p>
-                Wykonana z wysokiej jakości materiałów, podstawka jest trwała i
-                łatwa do czyszczenia. Jej nowoczesny design sprawia, że
-                doskonale komponuje się z każdym wnętrzem.
-              </p>
-              <p>
-                Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą i
-                funkcjonalnością, które ułatwią Ci codzienne gotowanie.
-              </p>
-              <p>
-                Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
-                ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
-                Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
-                przestrzeni roboczej.
-              </p>
-              <p>
-                Wykonana z wysokiej jakości materiałów, podstawka jest trwała i
-                łatwa do czyszczenia. Jej nowoczesny design sprawia, że
-                doskonale komponuje się z każdym wnętrzem.
-              </p>
-              <p>
-                Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą i
-                funkcjonalnością, które ułatwią Ci codzienne gotowanie.
-              </p>{" "}
+
+              <div className="details__sticky-product">
+                <Product
+                  key={1}
+                  product={products[1]}
+                  className="assortment__container__products__item"
+                  onMenuChange={setOnMenuVisible}
+                  inDetails={true}
+                  setOrderModalVisible={(visible) =>
+                    setOrderModalProductId(visible ? products[1].id : null)
+                  }
+                  orderModalVisible={orderModalProductId === products[1].id}
+                  style={
+                    onMenuVisible || orderModalProductId !== null
+                      ? { opacity: "0.8" }
+                      : {}
+                  }
+                />
+              </div>
+              <div className="details__container__content__text">
+                <p>
+                  Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
+                  ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
+                  Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
+                  przestrzeni roboczej.
+                </p>
+                <p>
+                  Wykonana z wysokiej jakości materiałów, podstawka jest trwała
+                  i łatwa do czyszczenia. Jej nowoczesny design sprawia, że
+                  doskonale komponuje się z każdym wnętrzem.
+                </p>
+                <p>
+                  Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą
+                  i funkcjonalnością, które ułatwią Ci codzienne gotowanie.
+                </p>
+                <p>
+                  Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
+                  ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
+                  Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
+                  przestrzeni roboczej.
+                </p>
+                <p>
+                  Wykonana z wysokiej jakości materiałów, podstawka jest trwała
+                  i łatwa do czyszczenia. Jej nowoczesny design sprawia, że
+                  doskonale komponuje się z każdym wnętrzem.
+                </p>
+                <p>
+                  Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą
+                  i funkcjonalnością, które ułatwią Ci codzienne gotowanie.
+                </p>
+                <p>
+                  Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
+                  ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
+                  Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
+                  przestrzeni roboczej.
+                </p>
+                <p>
+                  Wykonana z wysokiej jakości materiałów, podstawka jest trwała
+                  i łatwa do czyszczenia. Jej nowoczesny design sprawia, że
+                  doskonale komponuje się z każdym wnętrzem.
+                </p>
+                <p>
+                  Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą
+                  i funkcjonalnością, które ułatwią Ci codzienne gotowanie.
+                </p>
+                <p>
+                  Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
+                  ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
+                  Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
+                  przestrzeni roboczej.
+                </p>
+                <p>
+                  Wykonana z wysokiej jakości materiałów, podstawka jest trwała
+                  i łatwa do czyszczenia. Jej nowoczesny design sprawia, że
+                  doskonale komponuje się z każdym wnętrzem.
+                </p>
+                <p>
+                  Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą
+                  i funkcjonalnością, które ułatwią Ci codzienne gotowanie.
+                </p>
+                <p>
+                  Podstawka pod Thermomix TSv3 to idealne rozwiązanie dla osób
+                  ceniących sobie porządek i estetykę w kuchni. Dzięki niej Twój
+                  Thermomix będzie zawsze na swoim miejscu, a Ty zyskasz więcej
+                  przestrzeni roboczej.
+                </p>
+                <p>
+                  Wykonana z wysokiej jakości materiałów, podstawka jest trwała
+                  i łatwa do czyszczenia. Jej nowoczesny design sprawia, że
+                  doskonale komponuje się z każdym wnętrzem.
+                </p>
+                <p>
+                  Dzięki podstawce pod Thermomix TSv3 możesz cieszyć się wygodą
+                  i funkcjonalnością, które ułatwią Ci codzienne gotowanie.
+                </p>{" "}
+              </div>
             </div>
           </div>
-        </div>
+        </ScrollEffectContainer>
+        {/* MODAL */}
+        {modalOpen && (
+          <div
+            className={`carousel__modal${
+              zoomed ? " carousel__modal--zoomed" : ""
+            }`}
+            onClick={handleModalClick}
+          >
+            <button
+              className="carousel__modal__close-btn"
+              onClick={() => {
+                setModalOpen(false);
+                setZoomed(false);
+              }}
+              aria-label="Zamknij"
+            >
+              &times;
+            </button>
+            <div>
+              <Carousel
+                items={items}
+                initialIndex={modalIndex}
+                isModal
+                onItemClick={() => setZoomed((z) => !z)}
+                zoomed={zoomed}
+                onResetZoom={() => setZoomed(false)}
+                onClose={() => {
+                  setModalOpen(false);
+                  setZoomed(false);
+                }}
+              />
+            </div>
+          </div>
+        )}
       </section>
       <Footer />
       <CookieBaner />
